c7da2b281d85eb3dd3345a63523083465821eb97
ZhanuzakovAsset <135130366+ZhanuzakovAsset@users.noreply.github.com>
2023-06-04 07:56:22 +0600

    SecondCest.php

---
+++ SecondCest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class SecondCest
+{
+    public function _before(AcceptanceTester $I)
+    {   $I->amOnPage('/home');
+		$I->waitForText('Войти');
+        $I->click('body button:nth-child(3)');
+		$I->waitForText('Вход');
+		if ($I->seeElement('body button:nth-child(4)')) {
+            $I->click('body button:nth-child(4)');
+        };
+		$I->fillField('input[name=email]', 'aset060199@mail.ru');
+		$I->fillField('input[name=password]', 'Killer060199@');
+		$I->click('button[type=submit]');
+		$I->waitForText('Документы');
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {   
+		$I->click('nav button:nth-child(4)');		
+		$I->waitForText('Информация');
+        $I->click('main button:nth-child(2)');
+		$I->waitForText('Настройки профиля');
+		$I->click('a[href="/profile/edit"]');
+		$I->waitForText('Сохранить');
+		$I->fillField('input[name=sLastName]', 'Жанузаков');
+		$I->fillField('input[name=sName]', 'Асет');
+		$I->click('//button/span[text()="Сохранить"]');
+		$I->wait(5);
+    }
+}
